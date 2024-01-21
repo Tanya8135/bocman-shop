d530b3260c76a1b8efa6fc8deb5205d59b6c0a67
Tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2024-01-21 19:10:42 +0000

    Deploying to gh-pages from @ Tanya8135/bocman-shop@fd785cfc5452f0cc5a01d04c8b6cc56861f2b73f 🚀

--- index.0e77a4e0.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){document.querySelectorAll(".data-field-text").forEach(function(t){t.addEventListener("input",function(){t.classList.toggle("data-field-text",""!==t.value.trim())})})});
-//# sourceMappingURL=index.0e77a4e0.js.map
