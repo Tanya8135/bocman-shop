6220c9b7c507e831f553dd259bf05ac0245df229
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2024-02-03 23:01:17 +0200

    add scroll on the btnBack

--- src/js/catalog/loadMore.js
+++ src/js/catalog/loadMore.js
@@ -6,37 +6,46 @@ const loadMoreBtn = document.querySelector('.btn-loadMore');
 const itemsToShow = 8;
 let itemShow = 0;
 
-function showMoreTems() {
+function showMoreItems() {
     const additionalItems = listPoufs.slice(itemShow, itemShow + itemsToShow);
 
-    const itemsAdd = listPoufs.map(item => {
+    const itemsToAdd = additionalItems.map(item => {
         const li = document.createElement('li');
         li.className = 'catalog-item';
 
         const article = document.createElement('article');
         article.innerHTML = `
-    <img
-    srcset="${item.srcset}"
-    sizes="${item.sizes}"
-    src="${item.src}"
-    alt="${item.alt}"
-  />
-    `;
+            <img
+                srcset="${item.srcset}"
+                sizes="${item.sizes}"
+                src="${item.src}"
+                alt="${item.alt}"
+            />
+        `;
 
         li.appendChild(article);
         return li;
     });
 
-    existingUl.append(...itemsAdd);
+    existingUl.append(...itemsToAdd);
 
     itemShow += itemsToShow;
 
     if (itemShow >= listPoufs.length) {
         loadMoreBtn.style.display = 'none';
+        const btnBack = document.createElement('button');
+        existingUl.append(btnBack);
+        btnBack.className = 'testbutton';
+        btnBack.innerHTML = `
+            <svg width="30" height="30" class="arrow-back">
+                <use href="./images/icons.svg#icon-arrow-back"></use>
+            </svg>
+        `;
+
+        btnBack.addEventListener('click', () => {
+            existingUl.scrollIntoView({ behavior: 'smooth' });
+        });
     }
 }
 
-loadMoreBtn.addEventListener('click', showMoreTems);
-
-
-
+loadMoreBtn.addEventListener('click', showMoreItems);
